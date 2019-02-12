f91fdc95db389024b3ed370367ef13652345e21a
tusharkaley <tomcat1903>
2019-02-12 14:43:54 -0500

    Going back to level 0 and just sanity checking

--- P1/BigQ.cc
+++ P1/BigQ.cc
@@ -9,23 +9,23 @@ struct worker_data{
 	int run_length;
 };
 void *sort_tpmms (void *arg) {
-	struct worker_data * input_args = (worker_data *)arg;
-	Record newinrec; 
+	// struct worker_data * input_args = (worker_data *)arg;
+	// Record newinrec; 
 
-	Record newrec[2];
-	Record *newlast = NULL, *newprev = NULL;
-	int err = 0;
-	int i = 0;
-	Schema mySchema ("catalog", "nation"); 
+	// Record newrec[2];
+	// Record *newlast = NULL, *newprev = NULL;
+	// int err = 0;
+	// int i = 0;
+	// Schema mySchema ("catalog", "nation"); 
 
-	while (input_args->in_pipe->Remove (&newrec[i%2])) {
-		newprev = newlast;
-		newlast = &newrec[i%2];
-		input_args->out_pipe->Insert(newlast);
-		i++;
-	}
+	// while (input_args->in_pipe->Remove (&newrec[i%2])) {
+	// 	newprev = newlast;
+	// 	newlast = &newrec[i%2];
+	// 	input_args->out_pipe->Insert(newlast);
+	// 	i++;
+	// }
 	cout << " Worker doing some work here"<<endl;
-	pthread_exit(NULL);
+	//pthread_exit(NULL);
 }
 
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
@@ -36,18 +36,18 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
 	input.out_pipe = &out;
 	input.sort_order = &sortorder;
 	input.run_length = runlen;
-	//THIS WORKS. READS RECORDS FROM IN PIPE AND PRINTS THEM OUT
-	// Record rec[2];
-	// Record *last = NULL, *prev = NULL;
-	// int err = 0;
-	// int i = 0;
-	// Schema mySchema ("catalog", "nation"); 
- 	// while (in.Remove(&rec[i%2])) {
-	// 	prev = last;
-	// 	last = &rec[i%2];
-	// 	last->Print(&mySchema);
-	// 	i++;
-	// }
+	// THIS WORKS. READS RECORDS FROM IN PIPE AND PRINTS THEM OUT
+	Record rec[2];
+	Record *last = NULL, *prev = NULL;
+	int err = 0;
+	int i = 0;
+	Schema mySchema ("catalog", "nation"); 
+ 	while (in.Remove(&rec[i%2])) {
+		prev = last;
+		last = &rec[i%2];
+		last->Print(&mySchema);
+		i++;
+	}
 	cout << "Inside BIGQ"<<endl;
     // construct priority queue over sorted runs and dump sorted data 
  	// into the out pipe
