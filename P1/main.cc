de9149c64b1bdcd090c3b79851ea3e11340ad76e
tusharkaley <tomcat1903>
2019-02-15 18:25:25 -0500

    Fixed errors with main

--- P1/main.cc
+++ P1/main.cc
@@ -85,125 +85,14 @@ void *consumer (void *arg) {
 	}
 }
 
-void merge(Record arr [], int l, int m, int r, OrderMaker sort_order) 
-{ 
-    int i, j, k; 
-    int n1 = m - l + 1; 
-    int n2 =  r - m; 
-	int val=0;
-	ComparisonEngine ceng;
-    /* create temp arrays */
-    Record L[n1], R[n2]; 
-	Schema mySchema ("catalog", "nation");
-    /* Copy data to temp arrays L[] and R[] */
-	// cout<<"L i initial state"<<endl;
-    for (i = 0; i < n1; i++) 
-        L[i] = arr[l + i]; 
-		// arr[l + i].Print(&mySchema);
-	// cout<<"R j initial state"<<endl;
-    for (j = 0; j < n2; j++) 
-        R[j] = arr[m + 1+ j]; 
-		// arr[m + 1+ j].Print(&mySchema);
-	
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray 
-    j = 0; // Initial index of second subarray 
-    k = l; // Initial index of merged subarray 
-    while (i < n1 && j < n2) 
-    { 
-		val = ceng.Compare (&L[i], &R[j], &sort_order);
-		
-        if (val != 1)
-        { 
-            arr[k] = L[i]; 
-            i++; 
-        } 
-        else
-        { 
-            arr[k] = R[j]; 
-            j++; 
-        } 
-		k++; 
 
-    }
 
-	
-	
-  
-    /* Copy the remaining elements of L[], if there 
-       are any */
-    while (i < n1) 
-    { 
-        arr[k] = L[i]; 
-        i++; 
-        k++; 
-    } 
-  
-    /* Copy the remaining elements of R[], if there 
-       are any */
-    while (j < n2) 
-    { 
-        arr[k] = R[j]; 
-        j++; 
-        k++; 
-    } 
-	// for(int i =0; i < 25;i++){
-	// 	arr[i].Print(&mySchema);
-	// }
-	// cout<< "**********************************" << endl;
-} 
-  
-/* l is for left index and r is right index of the 
-   sub-array of arr to be sorted */
-void mergeSort(Record arr[], int l, int r, OrderMaker sort_order) 
-{ 
-    if (l < r) 
-    { 
-        // Same as (l+r)/2, but avoids overflow for 
-        // large l and h 
-        int m = l+(r-l)/2; 
-  
-        // Sort first and second halves 
-        mergeSort(arr, l, m, sort_order); 
-        mergeSort(arr, m+1, r, sort_order); 
-  
-        merge(arr, l, m, r, sort_order); 
-    } 
-}
 
-  
-/* Driver program to test above functions */
-//Function to test the mergesort function
-// int main() 
-// { 
-// 	Schema mySchema ("catalog", "nation");
-// 	OrderMaker sortorder(&mySchema);
-// 	cout << "\n specify sort ordering (when done press ctrl-D):\n\t ";
-
-// 	vector<Record> vec_arr; 
-
-// 	DBFile dbfile;
-// 	dbfile.Open ("nation.bin");
-	
-// 	dbfile.MoveFirst ();
-// 	int counter = 0;
-// 	Record inprec;
-// 	while (dbfile.GetNext(inprec) == 1) {
-// 		counter += 1;
-// 		vec_arr.push_back(inprec);
-// 	}
-// 	dbfile.Close ();
-
-//     int arr_size = vec_arr.size(); 
-// 	reverse(vec_arr.begin(),vec_arr.end());
-// 	Record arr[arr_size];
-//     mergeSort(arr, 0, arr_size - 1, sortorder); 
-//     return 0; 
-// } 
 
 int main () {
 
 	// sort order for records
+	Schema mySchema ("catalog", "nation");
 	OrderMaker sortorder(&mySchema);
 	
 	// Logic to get sort order input from the user
@@ -220,11 +109,11 @@ int main () {
 	// sort_pred.GrowFromParseTree (final, &mySchema, literal); // constructs CNF predicate
 	// OrderMaker dummy;
 	// sort_pred.GetSortOrders (sortorder, dummy);
-	// int runlen =10;
+	int runlen =10;
 	// int option = 1;
-	// int buffsz = 100; // pipe cache size
-	// Pipe input (buffsz);
-	// Pipe output (buffsz);
+	int buffsz = 100; // pipe cache size
+	Pipe input (buffsz);
+	Pipe output (buffsz);
 
 	// thread to dump data into the input pipe (for BigQ's consumption)
 	// pthread_t thread1;
@@ -249,19 +138,19 @@ int main () {
 
 	cout << " producer: inserted " << counter << " recs into the pipe\n";
 	// thread to read sorted data from output pipe (dumped by BigQ)
-	// pthread_t thread2;
-	// testutil tutil = {&output, &sortorder, false, false};
+	pthread_t thread2;
+	testutil tutil = {&output, &sortorder, false, false};
 	// if (option == 2) {
 	// 	tutil.print = true;
 	// }
 	// else if (option == 3) {
 	// 	tutil.write = true;
 	// }
-	// pthread_create (&thread2, NULL, consumer, (void *)&tutil);
+	pthread_create (&thread2, NULL, consumer, (void *)&tutil);
 
 	BigQ bq (input, output, sortorder, runlen);
 
 	// pthread_join (thread1, NULL);
-	// pthread_join (thread2, NULL);
+	pthread_join (thread2, NULL);
 	return 0;
 }
