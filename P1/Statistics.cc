0b81d28896782b4a997f0c8c382da43694c5bb53
Sharmilee <sharmileedesai@ufl.edu>
2019-04-08 16:38:13 -0400

    addrel and addAtt

--- P1/Statistics.cc
+++ P1/Statistics.cc
@@ -1,7 +1,14 @@
 #include "Statistics.h"
+#include <unordered_map>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
 Statistics::Statistics()
 {
+    // relationMap 
+    
+
 }
 Statistics::Statistics(Statistics &copyMe)
 {
@@ -10,11 +17,54 @@ Statistics::~Statistics()
 {
 }
 
+
 void Statistics::AddRel(char *relName, int numTuples)
 {
+    //if new realtion add to the structure
+    if(relationMap.find(relName) == relationMap.end()) {
+        relation_struct relstruct;
+        relstruct.num_tuples = numTuples;
+        relationMap.insert({relName, relstruct});
+    }
+    //if relation already exists update the value 
+    else {
+        relationMap.at(relName).num_tuples = numTuples;
+    }
+
 }
 void Statistics::AddAtt(char *relName, char *attName, int numDistincts)
 {
+     //if new relation add a new entry with numTuples = numDistincts
+     if(relationMap.find(relName) == relationMap.end()){
+        relation_struct relstruct;
+        relstruct.num_tuples = numDistincts;
+        relstruct.innerMap.insert({attName, numDistincts});
+        relationMap.insert({relName, relstruct});
+    } 
+    //if relation aready exists
+    else {
+        // relation_struct restruct = this->relationMap.find(relName)->second; 
+        //if its a new attribute
+        if(this->relationMap.find(relName)->second.innerMap.find(attName) == this->relationMap.find(relName)->second.innerMap.end())
+        {
+            if(numDistincts != -1)
+                this->relationMap.find(relName)->second.innerMap.insert({attName, numDistincts});
+            else
+                this->relationMap.find(relName)->second.innerMap.insert({attName, this->relationMap.find(relName)->second.num_tuples});
+            
+        }
+        //if the attribute is  already present, update distinctValues
+        else
+        {
+            if(numDistincts != -1)
+                this->relationMap.find(relName)->second.innerMap.at(attName)= this->relationMap.find(relName)->second.num_tuples;
+                // insert({attName, numDistincts});
+            else
+                this->relationMap.find(relName)->second.innerMap.at(attName)= numDistincts;
+        }
+        
+
+    }
 }
 void Statistics::CopyRel(char *oldName, char *newName)
 {
--- P1/Statistics.h
+++ P1/Statistics.h
@@ -1,11 +1,23 @@
 #ifndef STATISTICS_
 #define STATISTICS_
 #include "ParseTree.h"
+#include <unordered_map>
+#include <string>
 
+using namespace std;
+// typedef unordered_map<char*, int> attributeMap;
+// typedef unordered_map<char *, pair<int, attributeMap>>  relationMap;
+struct  relation_struct{
+	int num_tuples;
+	unordered_map<char*, int> innerMap;
+};
 
 class Statistics
 {
-public:
+	public:
+	// struct relation_struct * rel_struct;
+	unordered_map<char *, struct relation_struct>  relationMap;	
+	// unordered_map<int , int> relationMap;
 	Statistics();
 	Statistics(Statistics &copyMe);	 // Performs deep copy
 	~Statistics();
