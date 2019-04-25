e13351f5470c6b67fc94467a9ff5e0cae7305740
tusharkaley <tomcat1903>
2019-04-25 16:55:03 -0400

    Added all the extern variables

--- P1/main.cc
+++ P1/main.cc
@@ -7,10 +7,22 @@ using namespace std;
 extern "C" {
 	int yyparse(void);   // defined in y.tab.c
 }
+extern struct TableList *tables;
+extern struct NameList *groupingAtts;
+extern struct AndList *boolean;
+extern  struct NameList *attsToSelect;
+extern int distinctAtts;
+extern int distinctFunc;
 
 int main () {
-
+	cout<<"Enter the query"<<endl;
 	yyparse();
+	cout<<"Table name "<<tables->tableName<<endl;
+	cout<<"Name list grouping atts "<<groupingAtts->name<<endl;
+	cout<<"Name list attsToSelect "<<attsToSelect->name<<endl;
+	cout<<"Distinct Atts "<<distinctAtts<<endl;
+	cout<<"Distinct Funcs "<<distinctFunc<<endl;
+
 }
 
 
