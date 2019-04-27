be2c6ce8ca41f3c4507eff81a23b8bb7e3cfa3cd
tusharkaley <tomcat1903>
2019-04-27 13:50:54 -0400

    Moved the generate select node function

--- P1/main.cc
+++ P1/main.cc
@@ -209,6 +209,41 @@ string getOperandFromCode(int code){
 				break;    
 		}
 }
+TreeNode generateSelectNode(vector <string> aliases, 
+                                      vector <string> aliasAs, 
+                                      int num_rels, 
+                                      vector <string> tableName,
+                                      unordered_map<string, string> aliasToRel){
+        if (num_rels==1){
+            SelectFile_node sf_node;
+            sf_node.node_type = "select";
+            // sf_node.selOp = 
+            // sf_node.literal =  
+            string rel_name;
+            std::sort(aliases.begin(), aliases.end());
+            string out_pipe = "";
+            for(int i =0; i<aliases.size(); i++){
+                out_pipe = out_pipe+ "_"+aliases[i];
+                rel_name = aliasToRel.at(aliases[i]);
+	        }
+            Schema outSch("catalog", (char*)rel_name.c_str());
+            sf_node.output_schema = &outSch;
+            sf_node.out_pipe_name = out_pipe;
+            return sf_node;
+        }
+        else{
+            SelectPipe_node sp_node;
+            std::sort(aliases.begin(), aliases.end());
+            string out_pipe = "";
+            for(int i =0; i<aliases.size(); i++){
+                out_pipe = out_pipe+ "_"+aliases[i];
+	        }
+            return sp_node;
+        }
+        
+
+}
+
 void getSelectFileNodes(vector <OrList*> selectionVector, 
 						vector <OrList*> &joinVector, 
 						vector <string> tableName, 
@@ -249,7 +284,7 @@ void getSelectFileNodes(vector <OrList*> selectionVector,
 		if (loop_ind==1){
 			selection.insert({alias, true});
 		}
-		select_node.generateSelectNode(temp_aliases, aliasAs, loop_ind, tableName, aliasToRel);
+		select_node = generateSelectNode(temp_aliases, aliasAs, loop_ind, tableName, aliasToRel);
 		operations_tree.insert({select_node.out_pipe_name, select_node});
 	}
 	string attNameLeft;
